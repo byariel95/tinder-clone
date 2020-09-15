e46c31a0b82b515fe34e7af9688760462a0590ba
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2020-09-14 20:14:22 -0600

    added header and router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from './header/Header';
+import {BrowserRouter as Router , Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>hola tinder</h1>   
+      <Header/> 
+      <Router>
+        <Switch>
+          <Route path='/'>
+            <h1>home page</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
