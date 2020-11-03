da8c178c1090d05157dfd1f1ebb076d819304bcb
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2020-11-03 16:15:38 -0600

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA_h-tHXYJsCR0-a-C_8MKw1Lx272bLb10",
-    authDomain: "tinder-clone-b12c1.firebaseapp.com",
-    databaseURL: "https://tinder-clone-b12c1.firebaseio.com",
-    projectId: "tinder-clone-b12c1",
-    storageBucket: "tinder-clone-b12c1.appspot.com",
-    messagingSenderId: "795858191697",
-    appId: "1:795858191697:web:73cfd5299653581b467f70",
-    measurementId: "G-08T8KR4QRW"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
